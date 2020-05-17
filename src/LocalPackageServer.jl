37e3ab2fc0a6ecbe74d1afce81a2675be43b6691
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2020-05-17 11:33:22 +0200

    Add module docstring.

--- src/LocalPackageServer.jl
+++ src/LocalPackageServer.jl
@@ -1,3 +1,12 @@
+"""
+# LocalPackageServer
+
+Implementation of a Julia Storage Server for a single registry, where
+the registry and the registered packages are dynamically retrieved
+from their git repositories. It can also be configured as a
+low-feature Julia Package Server with a built in storage server.
+See the package's `README.md` for more information.
+"""
 module LocalPackageServer
 
 using Pkg
